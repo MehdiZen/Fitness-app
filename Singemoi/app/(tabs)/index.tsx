50696d3e04085a021cef75061fdca59b1eb02dea
Zenati Mehdi <mehdi1.zenati@epitech.eu>
2024-07-24 17:33:58 +0200

    Finalisation des recherches de macro nutriments, ajout du scan codebar

--- Singemoi/app/(tabs)/index.tsx
+++ Singemoi/app/(tabs)/index.tsx
@@ -7,32 +7,71 @@ import {
   Button,
   View,
   FlatList,
-  Pressable
+  Pressable,
+  Image,
+  Modal
 } from 'react-native';
 
 const App = () => {
+  interface FoodDetails {
+    food_name: string;
+    nf_calories?: number;
+    nf_total_fat?: number;
+    nf_protein?: number;
+    nf_total_carbohydrate?: number;
+  }
+
+
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
+  const [food, setFood] = useState<FoodDetails | null>(null);
+  const [isModalVisible, setModalVisible] = useState(false);
+
 
   const fetchData = async () => {
     try {
       const response = await fetch(`https://trackapi.nutritionix.com/v2/search/instant?query=${query}`, {
         headers: {
-          'x-app-id': '1d4750fc', 
+          'x-app-id': '1d4750fc',
           'x-app-key': 'c9c83622c701dc845c112fd353748592',
         },
       });
       const data = await response.json();
       setResults(data.common);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchSelected = async (foodName: string) => {
+    try {
+      const response = await fetch(`https://trackapi.nutritionix.com/v2/natural/nutrients`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-app-id': '1d4750fc',
+          'x-app-key': 'c9c83622c701dc845c112fd353748592',
+        },
+        body: JSON.stringify({ query: foodName }),
+      });
+      const data = await response.json();
+      setFood(data.foods[0]);
+      setModalVisible(true);
     } catch (error) {
       console.error(error);
     }
   };
 
   const renderItem = ({ item }) => (
-    <View style={styles.item}>
+    <Pressable style={styles.item} onPress={() => fetchSelected(item.food_name)}>
+      <Image
+        style={{width: 50, height: 50}} 
+        source={{uri: item.photo.thumb}}
+      />
+
       <Text style={styles.title}>{item.food_name}</Text>
-    </View>
+    </Pressable>
   );
 
   return (
@@ -51,8 +90,25 @@ const App = () => {
       <FlatList
         data={results}
         renderItem={renderItem}
-        keyExtractor={(item) => item.food_name}
-      />
+        keyExtractor={(item) => item.food_name} />
+
+      <Modal visible={isModalVisible}>
+        <View style={styles.modalContent}>
+          {food ? (
+            <>
+              <Text style={styles.modalTitle}>{food.food_name}</Text>
+              {food.nf_calories && <Text style={styles.modalElement}>Calories: {food.nf_calories}</Text>}
+              {food.nf_total_fat && <Text style={styles.modalElement}>Total Fat: {food.nf_total_fat}g</Text>}
+              {food.nf_protein && <Text style={styles.modalElement}>Protein: {food.nf_protein}g</Text>}
+              {food.nf_total_carbohydrate && <Text style={styles.modalElement}>Total Carbohydrate: {food.nf_total_carbohydrate}g</Text>}
+            </>
+          ) : (<Text>Loading...</Text>)}
+          <Pressable style={styles.closeButton} onPress={() => setModalVisible(false)}>
+            <Text style={styles.buttonText}>Close</Text>
+          </Pressable>
+        </View>
+      </Modal>
+
     </SafeAreaView>
   );
 };
@@ -60,9 +116,9 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 20,
+    marginTop: 25,
     padding: 16,
-    backgroundColor: '#fff',
+    backgroundColor: '#b6e0bf',
   },
   header: {
     fontSize: 24,
@@ -89,13 +145,49 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   item: {
+    display: 'flex',
+    flexDirection: 'row',
     padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
   },
   title: {
+    fontWeight: 'bold',
     fontSize: 18,
+    paddingLeft: 10,
+    display: 'flex',
+    alignItems: 'center'
   },
+  modalContent: {
+    height: '100vh',
+    backgroundColor: 'white',
+    padding: 22,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+ 
+  },
+  modalTitle: {
+    fontSize: 44,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    marginBottom: 26,
+  },
+  modalElement: {
+   fontSize: 26,
+    fontWeight: 'bold',
+  },
+  closeButton: {
+    backgroundColor: '#1E90FF',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 16,
+  },
+  foodLogo: {
+    width: 50,
+    height: 50
+  }
 });
 
 export default App;
---
+++ Singemoi/app/(tabs)/scan.tsx
@@ -0,0 +1,85 @@
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
+  return (
+    <View style={styles.container}>
+      <CameraView   
+      barcodeScannerSettings={{barcodeTypes: ["upc_a", "ean13"],}} 
+      style={styles.camera} facing={facing}
+      >
+        <Text style={styles.title}>Scan the barcode</Text>
+        <View style={styles.buttonContainer}>
+            <Text style={styles.rectangle}></Text>
+        </View>
+      </CameraView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    position: 'absolute',
+    left: 120,
+    top: 225,
+    color: 'white',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  rectangle: {
+    position: 'absolute',
+    width: 230,
+    height: 150,
+    top: 200,
+    borderWidth: 5,
+    borderColor: 'grey'
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
